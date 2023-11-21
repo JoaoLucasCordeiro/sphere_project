17ca1c4393e1b3ad72fd1ef90fd906fd0ad465c3
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-11-21 16:36:30 -0300

    feat: bottombar was added to the layout

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { bottombarLinks } from "@/constants";
+import { Link, useLocation } from "react-router-dom"
 
 const BottomBar = () => {
+
+
+  const { pathname } = useLocation()
+
   return (
-    <div>BottomBar</div>
+    <section className='bottom-bar'>
+      {bottombarLinks.map((link) => {
+
+        const isActive = pathname === link.route;
+
+        return (
+
+          <Link
+            to={link.route}
+            className={`${isActive && 'bg-primary-500 rounded-[8px]'} flex-center flex-col gap-1 p-2 transition`} key={link.label}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              width={16}
+              height={16}
+              className={`group-hover:invert-white ${isActive && 'invert-white'}`}
+            />
+
+            <p className="tiny-medium text-light-2">{link.label}</p>
+            
+
+          </Link>
+
+        )
+      })}
+    </section>
   )
 }
 
