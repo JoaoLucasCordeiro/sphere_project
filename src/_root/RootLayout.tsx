3026856dcfd3c9520396c04ccc2efd02bd2d7150
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-11-21 15:38:22 -0300

    feat: setup the structure of the rootlayout

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,6 +1,21 @@
+import BottomBar from "@/components/shared/BottomBar"
+import LeftsideBar from "@/components/shared/LeftSideBar"
+import Topbar from "@/components/shared/Topbar"
+import { Outlet } from "react-router-dom"
+
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftsideBar/>
+
+      <section className="flex flex-1 h-full">
+        <Outlet/>
+      </section>
+
+
+      <BottomBar/>
+    </div>
   )
 }
 
