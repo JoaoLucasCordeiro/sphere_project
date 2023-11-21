81d9aa5c096216dc7dd222b4a5b84c91a492b3ce
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-11-21 15:37:30 -0300

    chore: the constants are added to use in the leftside bar and topbar

---
+++ src/constants/index.ts
@@ -0,0 +1,50 @@
+export const sidebarLinks = [
+    {
+      imgURL: "/assets/icons/home.svg",
+      route: "/",
+      label: "Início",
+    },
+    {
+      imgURL: "/assets/icons/wallpaper.svg",
+      route: "/explore",
+      label: "Explorar",
+    },
+    {
+      imgURL: "/assets/icons/people.svg",
+      route: "/all-users",
+      label: "Pessoas",
+    },
+    {
+      imgURL: "/assets/icons/bookmark.svg",
+      route: "/saved",
+      label: "Salvos",
+    },
+    {
+      imgURL: "/assets/icons/gallery-add.svg",
+      route: "/create-post",
+      label: "Novo Post",
+    },
+  ];
+  
+  export const bottombarLinks = [
+    {
+      imgURL: "/assets/icons/home.svg",
+      route: "/",
+      label: "Início",
+    },
+    {
+      imgURL: "/assets/icons/wallpaper.svg",
+      route: "/explore",
+      label: "Explorar",
+    },
+    {
+      imgURL: "/assets/icons/bookmark.svg",
+      route: "/saved",
+      label: "Salvos",
+    },
+    {
+      imgURL: "/assets/icons/gallery-add.svg",
+      route: "/create-post",
+      label: "Novo Post",
+    },
+  ];
