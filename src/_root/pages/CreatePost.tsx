be26aaa578de60b1592b73c3ab6c35bc2b890280
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-11-21 18:10:56 -0300

    feat: upload image in the create post page it's now working

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import PostForm from "@/components/forms/PostForm"
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className="flex flex-1">
+
+      <div className="common-container">
+
+        <div className="max-w-5xl flex-start gap-3 justify-start w-full">
+          <img
+            src="/assets/icons/add-post.svg"
+            width={36}
+            height={36}
+            alt="Add post"
+          />
+          <div className="flex flex-col">
+          <h2 className="h3-bold md:h2-bold text-left w-full">Criar nova postagem</h2>
+          <span className="text-[12px]">O que há de novo no campus?</span>
+          </div>
+          
+        </div>
+
+        <PostForm/>
+
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/components/forms/PostForm.tsx
@@ -0,0 +1,123 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "../ui/textarea"
+import FileUploader from "../shared/FileUploader"
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+})
+
+
+// 2. Define a submit handler.
+function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+}
+
+
+const PostForm = ({ post }) => {
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
+                <FormField
+                    control={form.control}
+                    name="caption"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="shad-form_label">Legenda</FormLabel>
+                            <FormControl>
+                                <Textarea
+                                    className="shad-textarea custom-scrollbar"
+                                    {...field}
+                                />
+                            </FormControl>
+
+                            <FormMessage className="shad-form_message" />
+                        </FormItem>
+                    )}
+                />
+
+
+                <FormField
+                    control={form.control}
+                    name="file"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="shad-form_label">Adicione uma foto</FormLabel>
+                            <FormControl>
+                                <FileUploader
+                                    fieldChange={field.onChange}
+                                    mediaUrl={post?.imageUrl}
+                                />
+                            </FormControl>
+
+                            <FormMessage className="shad-form_message" />
+                        </FormItem>
+                    )}
+                />
+
+
+                <FormField
+                    control={form.control}
+                    name="location"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="shad-form_label">Adicone sua localização</FormLabel>
+                            <FormControl>
+                                <Input type="text" className="shad-input" placeholder="Biblioteca" />
+                            </FormControl>
+
+                            <FormMessage className="shad-form_message" />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="tags"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="shad-form_label">Adicone hashtags (com vírgulas "," entre cada uma)</FormLabel>
+                            <FormControl>
+                                <Input type="text" className="shad-input" placeholder="Aula, Novidade, Estágio" />
+                            </FormControl>
+
+                            <FormMessage className="shad-form_message" />
+                        </FormItem>
+                    )}
+                />
+
+                <div className="flex gap-4 items-center justify-end">
+                    <Button type="button" className="shad-button_dark_4">Cancelar</Button>
+                    <Button type="submit" className="shad-button_primary whitespace-nowrap">Postar</Button>
+                </div>
+
+            </form>
+        </Form>
+    )
+}
+
+export default PostForm
