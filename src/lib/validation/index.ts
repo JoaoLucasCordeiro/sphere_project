9555996fa9c3928b7b280b3c05d7fce622049e73
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-11-25 18:15:15 -0300

    chore: function helpers added

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -12,6 +12,15 @@ export const SignupValidation = z.object({
     password: z.string().min(8, {message: "A Senha Deve Possuir Pelo Menos 8 Caracteres"}),
   })
 
+  export const ProfileValidation = z.object({
+    file: z.custom<File[]>(),
+    name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+    username: z.string().min(2, { message: "Name must be at least 2 characters." }),
+    email: z.string().email(),
+    bio: z.string(),
+  });
+  
+
   export const PostValidation = z.object({
    caption: z.string().min(5).max(2200),
    file: z.custom<File[]>(),
