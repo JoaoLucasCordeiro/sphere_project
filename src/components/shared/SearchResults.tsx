ab1f455415cded0e9b3bc3e3be7f2e44d80330be
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-11-25 18:18:02 -0300

    feat: search result applied

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,6 +1,24 @@
-const SearchResults = () => {
+import { Models } from "appwrite"
+import Loader from "./Loader"
+import GridPostList from "./GridPostList"
+
+type searchResultsProps = {
+  isSearchFetching: boolean,
+  searchedPosts: Models.Document[]
+}
+
+const SearchResults = ({ isSearchFetching, searchedPosts }: searchResultsProps) => {
+
+  if (isSearchFetching) return <Loader />
+
+  if (searchedPosts && searchedPosts.documents.length > 0) {
+    return (
+      <GridPostList posts={searchedPosts.documents} />
+    )
+  }
+
   return (
-    <div>SearchResults</div>
+    <p className="text-light-4 mt-18 text-center w-full">Nenhum resultado encontrado</p>
   )
 }
 
