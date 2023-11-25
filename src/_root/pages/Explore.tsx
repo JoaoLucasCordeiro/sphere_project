e2cd2c2014c73ba829809b258f91fc5c7c1a771e
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-11-25 18:18:14 -0300

    feat: explore page it's now working good

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -4,15 +4,23 @@ import SearchResults from "@/components/shared/SearchResults"
 import { Input } from "@/components/ui/input"
 import useDebounce from "@/hooks/UseDebounce"
 import { useGetPosts, useSearchPosts } from "@/lib/react-query/queriesAndMutations"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useInView } from "react-intersection-observer"
+
 
 const Explore = () => {
+
+  const {ref, inView} = useInView()
 
   const { data: posts, fetchNextPage, hasNextPage } = useGetPosts()
 
   const [searchValue, setSearchValue] = useState('')
   const debouncedValue = useDebounce(searchValue, 500)
   const { data: searchedPosts, isFetching: isSearchFetching } = useSearchPosts(debouncedValue)
+
+  useEffect(() => {
+    if(inView && !searchValue) fetchNextPage()
+  }, [inView, searchValue])
 
   if (!posts) {
     return (
@@ -53,14 +61,23 @@ const Explore = () => {
 
       <div className="flex flex-wrap gap-9 w-full max-w-5xl">
         {shouldShowSearchResults ? (
-          <SearchResults />
+          <SearchResults
+            isSearchFetching={isSearchFetching}
+            searchedPosts={searchedPosts}
+          />
         ) : shouldShowPosts ? (
           <p className="text-light-4 mt-10 text-center w-full">Não há mais posts</p>
         ) : posts.pages.map((item, index) => (
-          <GridPostList key={`page-${index}`} post={item.documents} />
+          <GridPostList key={`page-${index}`} posts={item.documents} />
         ))}
       </div>
 
+
+      {hasNextPage && !searchValue && (
+        <div ref={ref} className="mt-10">
+            <Loader/>
+        </div>
+      )}
 
     </div>
   )
