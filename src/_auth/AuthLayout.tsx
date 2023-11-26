ced70f3c362d73a49b9ce81c3d49504720a2cef6
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-11-26 11:03:27 -0300

    fix: fix the querie function

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,8 @@
-import { Outlet, Navigate } from "react-router-dom"
+import { useUserContext } from "@/context/AuthContext";
+import { Outlet, Navigate } from "react-router-dom";
 
-const AuthLayout = () => {
-
-  const isAuthenticated = false;
+export default function AuthLayout() {
+  const { isAuthenticated } = useUserContext();
 
   return (
     <>
@@ -10,19 +10,17 @@ const AuthLayout = () => {
         <Navigate to="/" />
       ) : (
         <>
-          <section className="flex flex-1 items-center justify-center flex-col py-10">
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
 
           <img
             src="/assets/images/side-img.svg"
-            alt="Ilustration image"
+            alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
         </>
       )}
     </>
-  )
+  );
 }
-
-export default AuthLayout
--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,22 +1,23 @@
-import BottomBar from "@/components/shared/BottomBar"
-import LeftsideBar from "@/components/shared/LeftSideBar"
-import Topbar from "@/components/shared/Topbar"
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+
+import Topbar from "@/components/shared/Topbar";
+import LeftsideBar from "@/components/shared/LeftSideBar";
+import BottomBar from "@/components/shared/BottomBar";
+
 
 const RootLayout = () => {
   return (
     <div className="w-full md:flex">
       <Topbar />
-      <LeftsideBar/>
+      <LeftsideBar />
 
       <section className="flex flex-1 h-full">
-        <Outlet/>
+        <Outlet />
       </section>
 
-
-      <BottomBar/>
+      <BottomBar />
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
